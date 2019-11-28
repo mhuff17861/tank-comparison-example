46511f4479b28b0c174b5ed9781e47e5ecf9d67e
mhuff17861 <micahhuff1022010@gmail.com>
2019-11-28 01:04:41 -0500

    Added easyvim script to open all files in vim at once

--- C++_Assignment/main.cpp
+++ C++_Assignment/main.cpp
@@ -18,7 +18,7 @@ int main()
 	int id, ammo, count;
 	float speed;
 	clock_t milsec = 3000;
-		
+
 	try
 	{
 		cout << "Enter the first tank's' ID (0-50): \n";
@@ -27,80 +27,80 @@ int main()
 		cin >> speed;
 		cout << "Enter the first tank's' ammo (0-40): \n";
 		cin >> ammo;
-		
+
 		Tank one;
-		
+
 		one.setID(id);
 		one.setAmmo(ammo);
 		one.setSpeed(speed);
-		
+
 		cout << "Enter the second tank's' ID (0-50): \n";
 		cin >> id;
 		cout << "Enter the second tank's' speed (0-60): \n";
 		cin >> speed;
 		cout << "Enter the second tank's' ammo (0-40): \n";
 		cin >> ammo;
-		
+
 		Tank two(id, speed, ammo);
-		
+
 		outputTankInfo(one);
 		outputTankInfo(two);
 		sleep(milsec);
 		cout << endl;
-		
+
 		cout << "Increasing Speed\n";
 		one.increaseSpeed();
 		two.increaseSpeed();
-		
+
 		outputTankInfo(one);
 		outputTankInfo(two);
 		sleep(milsec);
 		cout << endl;
-		
+
 		cout << "Decreasing Speed\n";
 		one.decreaseSpeed();
 		two.decreaseSpeed();
-		
+
 		outputTankInfo(one);
 		outputTankInfo(two);
 		sleep(milsec);
 		cout << endl;
-		
+
 		do
 		{
 		    cout << "How many times would you like to fire? (1-25)\n";
 		    cin >> count;
 		} while (count < 1 || count > 25);
-		
+
 		cout << "Firing\n";
 		for (int i=0;i<count;i++)
 		{
 			one.fire();
 			two.fire();
 	    }
-	    
+
 	    outputTankInfo(one);
 		outputTankInfo(two);
 		sleep(milsec);
 		cout << endl;
-		
+
 		cout << "Reloading\n";
 		one.reload();
 		two.reload();
-		
+
 		outputTankInfo(one);
 		outputTankInfo(two);
 		sleep(milsec);
-		
+
 	} catch (const char* st)
 	{
 		cout << st << endl;
 	}
-	
+
 	cout << "Press enter to continue...\n";
 	cin.ignore();
 	cin.get();
-	
+
 	return 0;
 }
 
---
+++ easyvim.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Just here so I don't have to type this out everytime
+vim C++_Assignment/main.cpp C++_Assignment/Tank.cpp C++_Assignment/Tank.h JS_Assignment/main.js JS_Assignment/tank.js JS_Assignment/interface.html
+
